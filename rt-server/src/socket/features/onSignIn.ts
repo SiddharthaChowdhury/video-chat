f72d174c5fa4bf10aa0fe03e7d198a216709db44
Siddhartha <siddharthac6@gmail.com>
2020-09-06 23:51:59 +0200

    Cleaned up

--- rt-server/src/socket/features/onSignIn.ts
+++ rt-server/src/socket/features/onSignIn.ts
@@ -4,9 +4,8 @@ export const onSignIn = (client: any, clients: any) => {
     client.on(IdSocketKey.ClientCanIjoin, (userInfo: {id: number, name: string}) => {
         const userCount = Object.keys(clients).length;
 
-        if(userCount < 2 && !clients[userInfo.id]) {
+        if(userCount < 3 && !clients[userInfo.id]) {
             client.emit(IdSocketKey.CreatePeer);
-            console.log('create peer')
 
             client['myId'] = userInfo;
             clients[userInfo.id] = [client] // should containe userInfo
@@ -20,7 +19,6 @@ export const onSignIn = (client: any, clients: any) => {
 
             return;
         }
-        console.log(clients)
 
         client.emit(IdSocketKey.SessionFull);
     })
